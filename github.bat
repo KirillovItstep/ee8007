373dac02b77c93e183d9e073328d1d6fd8ef603b
KirillovItstep <kirillov.malp@gmail.com>
2022-04-18 18:34:36 +0300

    5ПМ.865.000ЭЭ8007 - раскладка с нумерацией и QR-кодом v.1.1

--- github.bat
+++ github.bat
@@ -1,7 +1,7 @@
 cd  "D:/Matlab/5ПМ.865.000ЭЭ8007/"
 git init
 git add .
-git commit -m "5ПМ.865.000ЭЭ8007 - раскладка с нумерацией и QR-кодом"
+git commit -m "5ПМ.865.000ЭЭ8007 - раскладка с нумерацией и QR-кодом v.1.1"
 git remote add origin https://github.com/KirillovItstep/ee8007.git
 git push -u origin
 
